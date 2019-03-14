c3753508b3492fa29218010e0fbcd7aaaa7f95c5
Bhavin Shah <BShah@beamsoftware.com>
2019-03-14 09:04:43 -0400

    14th March 2019 commit

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -20,14 +22,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Bhavin Shah'
+        name: 'Andrew Mead'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Bhavin Shah'
+        name: 'Andrew Mead'
     })
 })
 
@@ -35,41 +37,53 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Bhavin Shah'
+        name: 'Andrew Mead'
     })
 })
 
 app.get('/weather', (req, res) => {
-    
-    if(!req.query.address)
-    {
-        return res.send('Address is a must!')
-    }
-    
-    res.send({
-        forecast: 'It is snowing',
-        location:req.query.address
-    })
-})
-
-app.get('/products',(req,res)=>{
-
-    //on error
-    if(!req.query.search){
+    if (!req.query.address) {
         return res.send({
-            errorMessage:'You must Provide a Search Term'
+            error: 'You must provide an address!'
         })
     }
 
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
     res.send({
-        products:[]
+        products: []
     })
 })
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Bhavin Shah',
+        name: 'Andrew Mead',
         errorMessage: 'Help article not found.'
     })
 })
@@ -77,7 +91,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Bhavin Shah',
+        name: 'Andrew Mead',
         errorMessage: 'Page not found.'
     })
 })
