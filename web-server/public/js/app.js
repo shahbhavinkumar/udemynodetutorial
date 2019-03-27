2297f8d1ac17f80d33a0bf5d96b3b376c2ac54a3
Bhavin Shah <shahbhavinkumar@gmail.com>
2019-03-27 19:38:22 -0400

    Updating the UI with the URL

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,5 +1,24 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response)=>{
 
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#para1')
+
+
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    
+
+    fetch('http://localhost:3000/weather?address='+search.value).then((response)=>{
+
+    response.json().then((data)=>{
+        if(data.error){
+            message1.textContent="Server didnt send back a response"
+        }else{
+            message1.textContent=data.location;
+        }
+    })
+})
 })
